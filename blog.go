1c2e9a59c7c1d1b7c74391842dfe3fd7f5484a09
Aaron L <aaron@bettercoder.net>
2015-09-21 20:56:29 -0700

    Fix oauth2 library import.

--- blog.go
+++ blog.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	"gopkg.in/authboss.v0"
 	_ "gopkg.in/authboss.v0/auth"
 	_ "gopkg.in/authboss.v0/confirm"
@@ -61,7 +62,7 @@ func setupAuthboss() {
 				ClientID:     ``,
 				ClientSecret: ``,
 				Scopes:       []string{`profile`, `email`},
-				Endpoint:     aboauth.GoogleEndpoint,
+				Endpoint:     google.Endpoint,
 			},
 			Callback: aboauth.Google,
 		},
